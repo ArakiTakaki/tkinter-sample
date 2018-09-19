332cd34ebf6e6865cbad4b061391189a2fdd07ae
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-20 02:14:15 +0900

    push

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ root.title("Window Title")
 # Windowサイズの指定
 root.geometry("1010x1010")
 
+# 文字を配置する。
+label1 = tkinter.Label(root, text="Hallo")
+label1.pack(side="top")
+
+font1 = font.Font(family='Helvetica', size=20, weight='bold')
+label2 = tkinter.Label(root, text="Bye", bg="blue", font=font1)
+label2.pack(side="top")
+
+font2 = font.Font(family='Times', size=40)
+label2 = tkinter.Label(root, text="See you", fg="red", font=font2)
+label2.pack(side="top")
 
 # 実行
 root.mainloop()
