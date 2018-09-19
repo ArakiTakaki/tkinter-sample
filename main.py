13c03dfdeb9ac0474d85baab7a4376060aa2cfb6
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-20 02:13:34 +0900

    最低限の実装

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter
+from tkinter import font
+
+root = tkinter.Tk()
+
+# Windowのタイトル
+root.title("Window Title")
+
+# Windowサイズの指定
+root.geometry("1010x1010")
+
+
+# 実行
+root.mainloop()
